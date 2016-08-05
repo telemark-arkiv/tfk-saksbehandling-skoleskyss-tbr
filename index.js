4269482c9037b285678c018bdbece35b74bb88de
maccyber <jonas.enge@gmail.com>
2016-08-05 10:35:09 +0200

    Fixed paths

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ module.exports = function transportByTbr (options) {
   if (!options.husnummer) {
     throw new Error('Missing required input: options.husnummer')
   }
-
+  const basePath = require('path').resolve(__dirname)
   const isFile = require('is-file')
   const skoleid = parseInt(options.skoleid, 10)
-  const fileName = 'lib/data/' + skoleid + '.json'
-  const fileNameSpecials = 'lib/data/specials/' + skoleid + '.json'
+  const fileName = basePath + '/lib/data/' + skoleid + '.json'
+  const fileNameSpecials = basePath + '/lib/data/specials/' + skoleid + '.json'
   const postnummer = parseInt(options.postnummer, 10)
   const gatenavn = options.gatenavn
   const husnummer = parseInt(options.husnummer, 10)
 
   // Sjekk for specials ved gatenavn og husnummer
   if (isFile(fileNameSpecials)) {
-    const specials = require('./' + fileNameSpecials)
+    const specials = require(fileNameSpecials)
     if (specials[postnummer.toString()]) {
       var isSnowflake = false
       specials[postnummer.toString()].forEach(function (item) {
@@ -45,7 +45,7 @@ module.exports = function transportByTbr (options) {
 
   // Vanlig sjekk for postnummer
   if (isFile(fileName)) {
-    const zips = require('./' + fileName)
+    const zips = require(fileName)
     return zips.indexOf(postnummer) > -1
   } else {
     return false
